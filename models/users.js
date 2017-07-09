53467a730bdb51ab8ab2c1a3faebc3d993e21b18
Andogar <robert.andoga@gmail.com>
2017-07-09 17:34:08 -0400

    First commit, functionality mostly done. Need to implement json web token authentication for it

---
+++ models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+
+
+const usersSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    activities: [{
+        activityName: { type: String, required: true },
+        stats: [{
+            date: { type: String, required: true },
+            statAmount: { type: String, required: true }
+        }]
+    }]
+});
+ 
+const users = mongoose.model('Users', usersSchema);
+
+module.exports = users;
---
+++ server.js
@@ -0,0 +1,184 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const mongoose = require('mongoose');
+const users = require('./models/users');
+
+const application = express();
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/activityApplication');
+
+application.use(bodyParser.json());
+
+async function getUsers() {
+    var allUsers = await users.find();
+    return allUsers
+}
+
+application.get('/api/users', async (request, response) => {
+    var userList = await getUsers();
+    response.json(userList);
+});
+
+application.post('/api/users', async (request, response) => {
+    var name = request.body.name;
+    var password = request.body.password;
+    var email = request.body.email;
+    var userList = await getUsers();
+
+    var createUser = await users.create({
+        name: name,
+        password: password,
+        email: email
+    });
+
+    response.json(userList);
+});
+
+// LIST ACTIVITIES
+
+application.get('/api/users/:userId/activities', async (request, response) => {
+    var singleUser = await users.find({ _id: request.params.userId });
+    response.json(singleUser[0].activities);
+});
+
+// ADD ACTIVITIES
+
+application.post('/api/users/:userId/activities', async (request, response) => {
+    var activityName = request.body.activityName;
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    var addActivity = await singleUser[0].activities.push({ activityName: activityName });
+    var save = singleUser[0].save();
+
+    response.json(singleUser[0].activities);
+});
+
+// GET ONE ACTIVITY
+
+application.get('/api/users/:userId/activities/:activityId', async (request, response) => {
+    var activityId = request.params.activityId;
+
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    function findActivity(activity) {
+        return activity._id == activityId;
+    }
+
+    var singleActivity = await singleUser[0].activities.find(findActivity);
+
+    response.json(singleActivity);
+});
+
+// UPDATE ONE ACTIVITY NAME
+
+application.put('/api/users/:userId/activities/:activityId', async (request, response) => {
+    var activityId = request.params.activityId;
+
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    function findActivity(activity) {
+        return activity._id == activityId;
+    }
+
+    var singleActivity = await singleUser[0].activities.find(findActivity);
+    
+    singleActivity.activityName = request.body.activityName;
+    response.json(singleActivity);
+});
+
+// DELETE ONE ACTIVITY
+
+application.delete('/api/users/:userId/activities/:activityId', async (request, response) => {
+    var activityId = request.params.activityId;
+
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    function findActivity(activity) {
+        return activity._id == activityId;
+    }
+
+    var activityindex = await singleUser[0].activities.findIndex(findActivity);
+
+    var deleteActivity = singleUser[0].activities.splice(activityindex, 1);
+
+    response.json(singleUser[0].activities);
+});
+
+// GET STATS FOR ONE ACTIVITY
+
+application.get('/api/users/:userId/activities/:activityId/stats', async (request, response) => {
+    var activityId = request.params.activityId;
+
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    function findActivity(activity) {
+        return activity._id == activityId;
+    }
+
+    var singleActivity = await singleUser[0].activities.find(findActivity);
+
+    response.json(singleActivity.stats)
+});
+
+// ADD STATS FOR ONE ACTIVITY
+
+application.post('/api/users/:userId/activities/:activityId/stats', async (request, response) => {
+    var activityId = request.params.activityId;
+
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    function findActivity(activity) {
+        return activity._id == activityId;
+    }
+
+    var singleActivity = await singleUser[0].activities.find(findActivity);
+
+    var date = request.body.date;
+    var statAmount = request.body.statAmount;
+    function findStat(stat) {
+        return stat.date == date;
+    }
+
+    var statFind = singleActivity.stats.findIndex(findStat);
+
+    if(statFind != -1) {
+        singleActivity.stats[statFind] = {date: date, statAmount: statAmount};
+    } else {
+        singleActivity.stats.push({date: date, statAmount: statAmount});
+    }
+
+    var save = singleUser[0].save();
+
+    response.json(singleActivity.stats)
+});
+
+// DELETE ONE STAT FOR ONE ACTIVITY
+
+application.delete('/api/users/:userId/activities/:activityId/stats/:statId', async (request, response) => {
+    var activityId = request.params.activityId;
+    var statId = request.params.statId;
+    var singleUser = await users.find({ _id: request.params.userId });
+
+    function findActivity(activity) {
+        return activity._id == activityId;
+    }
+
+    var singleActivity = await singleUser[0].activities.find(findActivity);
+
+    function findStat(stat) {
+        return stat._id == statId;
+    }
+
+    var statIndex = singleActivity.stats.findIndex(findStat);
+
+    var deleteStat = singleActivity.stats.splice(statIndex, 1);
+
+    var save = singleUser[0].save();
+
+    response.json(singleActivity.stats)
+});
+
+
+application.listen(3000);
