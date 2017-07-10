14b810f144fba00278020792cc2eab00cc6474f3
Andogar <robert.andoga@gmail.com>
2017-07-09 23:11:00 -0400

    Added moment as an option for dates

--- models/users.js
+++ models/users.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const moment = require('moment');
-
 
 const usersSchema = new mongoose.Schema({
     name: { type: String, required: true },
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const moment = require('moment');
 const mongoose = require('mongoose');
 const users = require('./models/users');
 
